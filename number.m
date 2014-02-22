7499219f3ad1c7347209dea836f97c43185c35c0
helio <helionitial@me.com>
2014-02-22 04:37:29 -0600

    jk
    jk

--- number.m
+++ number.m
@@ -4,4 +4,69 @@
 /* creates and return an NSNumber object containing a given vlue, treating it as a BOOL
  */
 + (NSNumber *)numberWithBool:(BOOL)value
+/* create and return an NSNumber object containing a given value, treating it as a signed char
+ */
++ (NSNumber *)numberWithChar:(char)value
+/* create and return an NSNumber obejct containing a given value, treating it as a double 
+ */
++ (NSNumber *)numberWithDouble:(double)value
+/* create and return an NSNumber object containing a given value, treating it as a float.
+ */
++ (NSNumber *)numberWithFloat:(float)value
+/* create and return an NSNumber object containing a given value, treating it as a sisgned int 
+ */
++ (NSNumber *)numberWithInt:(int)value
+/* create and return an NSNumber obejct containing a given value, treating it as an NSInteger.
+ */
++ (NSNumber *)numberWithInteger:(NSInteger)value
+/* return the receiver's value as a BOOL */
+- (BOOL)boolValue
+/* return the receiver's value as a char */
+- (char)charValue
+/* return the receiver's value as a double */
+- (double)doubleValue
+/* return the receiver's value as a float */
+- (float)floatValue
+/* return the receiver's value as a NSInteger */
+- (NSInteger)integerValue
+/* return the receiver's value as an int. */
+- (int)intValue
+/* return the receiver's value as a human-readable string. */
+- (NSString *)stringValue
+
+
+/* Sample */
+#import <Foundation/Foundation.h>
+
+@interface SampleClass : NSObject
+- (NSNumber *)multiplyA:(NSNumber *)a withB:(NSNumber *)b;
+
+@end
+
+@implementation SampleClass
+
+- (NSNumber *)multiplyA:(NSNumber *a) withB:(NSNumber *)b{
+    float number1 = [a floatValue];
+    float number2 = [b floatValue];
+    float product = number1 * number2;
+    NSNumber *result = [NSNumber numberWithFloat:product];
+    
+    return result;
+}
+
+@end
+
+int main(){
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc]init];
+    
+    SampleClass *sampleClass = [[SampleClass alloc]init];
+    NSNumber *a = [NSNumber numberWithFloat:10.5];
+    NSNumber *b = [NSNumber numberWithFloat:10.0];
+    NSNumber *result = [sampleClass multiplyA:a withB :b];
+    NSString *resultString = [result stringValue];
+    NSLog(@"The product is %@", resultString);
+    
+    [pool drain];
+    return 0;
+}
 
