a45899e3970aff81703593efd04d429c8eb70a78
helio <helionitial@me.com>
2014-03-01 04:16:28 -0600

    helloworld
    ios7

---
+++ HelloWorld/HelloWorld/HelloWorldAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HelloWorldAppDelegate.h
+//  HelloWorld
+//
+//  Created by LiHe on 3/1/14.
+//  Copyright (c) 2014 LiHe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelloWorldAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HelloWorld/HelloWorld/HelloWorldViewController.h
@@ -0,0 +1,13 @@
+//
+//  HelloWorldViewController.h
+//  HelloWorld
+//
+//  Created by LiHe on 3/1/14.
+//  Copyright (c) 2014 LiHe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelloWorldViewController : UIViewController
+
+@end
